3f23fc8915f0cbf7749c08feece7cf8b008ff00f
Thomas <da_mekk@web.de>
2020-01-17 15:31:36 +0100

    small fixes

--- DAO.py
+++ DAO.py
@@ -4,7 +4,8 @@ IP_ADDRESS = "localhost"
 LOGIN = "admin"
 PASSWORD = "1234"
 PROF_TABLE = "users"
-VORLESUNG_TABLE = ""
+VORLESUNG_TABLE = "Vorlesung"
+BEWERTUNGEN_TABLE = "Bewertung"
 
 PROF_db = pymysql.connect(IP_ADDRESS, LOGIN, PASSWORD, PROF_TABLE)
 PROF_cursor = PROF_db.cursor()
@@ -13,12 +14,11 @@ VORLESUNG_db = pymysql.connect(IP_ADDRESS, LOGIN, PASSWORD, PROF_TABLE)
 VORLESUNG_cursor = VORLESUNG_db.cursor()
 
 
-def read_prof():
-    PROF_cursor.execute("SELECT * FROM user where name=%s", [text])
+def read_prof(prof_name):
+    PROF_cursor.execute("SELECT * FROM user where name=%s", prof_name)
     records = PROF_cursor.fetchall()
     for row in records:
-        PROF_cursor.execute("SELECT * FROM Vorlesung WHERE profID=%s", [row[0]])
-        vorlesungRec = PROF_cursor.fetchall()
+        return row[0]
     return 0;
 
 def create_vorlesung():
@@ -30,14 +30,21 @@ def update_vorlesung(vorlesungs_ID):
     return 0;
 
 def get_prof_name(prof_ID):
-
+    PROF_cursor.execute("SELECT * FROM user where ID=%s", prof_ID)
+    records = PROF_cursor.fetchall()
+    for row in records:
+        return row[1]
     return 0;
 
 def get_vorlesungen(prof_ID):
+    PROF_cursor.execute("SELECT * FROM Vorlesung WHERE profID=%s", [prof_ID])
+    return PROF_cursor.fetchall()
 
     return 0;
 
 def get_bewertungen(vorlesungs_ID):
+    PROF_cursor.execute("SELECT * FROM Bewertung WHERE vorlesungs_ID=%s", [vorlesungs_ID])
+    return PROF_cursor.fetchall()
 
     return 0;
 
