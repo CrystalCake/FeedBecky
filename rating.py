5ff4e6eba6627d60c1a1340117f7ec953e46c605
Thomas <da_mekk@web.de>
2020-01-28 16:13:26 +0100

    test rating

--- rating.py
+++ rating.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+import pymysql
+
 from DAO import *
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
+
+try:
+    print('no Card')
+    id, rating = reader.read()
+    print(id)
+    print(rating)
+    post_bewertung(rating, 26)
+
+#Übergabe an Interface
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
